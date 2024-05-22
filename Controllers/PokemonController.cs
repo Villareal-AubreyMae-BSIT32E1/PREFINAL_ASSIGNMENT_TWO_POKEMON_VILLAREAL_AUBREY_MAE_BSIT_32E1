c229ff9ce91240fb34159decd3ca2501e9ac01c7
user <user@DESKTOP-OMQ89VA>
2024-05-23 00:20:52 +0800

    Add project files.

---
+++ Controllers/PokemonController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PREFINAL_ASSIGNMENT_TWO_POKEMON.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System;
+
+namespace PREFINAL_ASSIGNMENT_TWO_POKEMON.Controllers
+{
+    public class PokemonController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public PokemonController(HttpClient httpClient)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+            _httpClient.BaseAddress = new Uri("https://pokeapi.co/api/v2/");
+        }
+
+        public async Task<IActionResult> Index(int page = 1)
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync($"pokemon?offset={(page - 1) * 20}&limit=20");
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsStringAsync();
+                var pokemonResponse = JsonConvert.DeserializeObject<PokemonResponse>(content);
+                return View(pokemonResponse.Results);
+            }
+            catch (HttpRequestException ex)
+            {
+                return StatusCode(500, $"Error retrieving Pokemon data: {ex.Message}");
+            }
+        }
+
+        public async Task<IActionResult> Details(string name)
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync($"pokemon/{name}");
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsStringAsync();
+                var pokemon = JsonConvert.DeserializeObject<Pokemon>(content);
+
+                return View(pokemon);
+            }
+            catch (HttpRequestException ex)
+            {
+                return StatusCode(500, $"Error retrieving Pokemon details: {ex.Message}");
+            }
+        }
+    }
+
+    internal class PokemonResponse
+    {
+        public string Results { get; internal set; }
+    }
+}
---
+++ Models/Pokemon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PREFINAL_ASSIGNMENT_TWO_POKEMON.Models{
+    public class Pokemon
+    {
+        public string Name { get; set; }
+        public System.Collections.Generic.List<string> Moves { get; set; }
+        public System.Collections.Generic.List<string> Abilities { get; set; }
+    }
+}
